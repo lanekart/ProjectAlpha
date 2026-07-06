87588e074e857ebdff6e638401c935e39cc3e45d
lanekart <rishabwahal@gmail.com>
2026-07-06 21:20:51 +0530

    Add research package boundary

---
+++ alpha/research/__init__.py
@@ -0,0 +1,19 @@
+"""Research platform domain APIs."""
+
+from alpha.research.walk_forward import (
+    WalkForwardEngine,
+    WalkForwardEvaluator,
+    WalkForwardReport,
+    WalkForwardResult,
+    WalkForwardWindow,
+    WalkForwardWindowGenerator,
+)
+
+__all__ = [
+    "WalkForwardEngine",
+    "WalkForwardEvaluator",
+    "WalkForwardReport",
+    "WalkForwardResult",
+    "WalkForwardWindow",
+    "WalkForwardWindowGenerator",
+]
