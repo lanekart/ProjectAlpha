458793ba384755cd36d8313053036ca48c3bf470
lanekart <rishabwahal@gmail.com>
2026-07-20 15:42:43 +0530

    Use standalone historical truth CLI entrypoint

--- alpha/historical_truth/__main__.py
+++ alpha/historical_truth/__main__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from alpha.application.historical_truth_cli import historical_truth_app
+from alpha.historical_truth.cli import historical_truth_app
 
 if __name__ == "__main__":
     historical_truth_app()
