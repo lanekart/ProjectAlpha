55bb00a5e4d009a8f3796690ff4ead358e9df1bd
lanekart <rishabwahal@gmail.com>
2026-07-21 01:11:37 +0530

    Export canonical replay consumer guard APIs

--- alpha/recovery/__init__.py
+++ alpha/recovery/__init__.py
@@ -8,6 +8,13 @@ from .canonical_replay import (
     CanonicalReplayStatus,
     canonical_replay_sha256,
 )
+from .consumer_attestation import (
+    CONSUMER_CONTRACT_VERSION,
+    CanonicalReplayConsumerAttestation,
+    export_consumer_attestations,
+)
+from .consumer_guard import CanonicalReplayConsumerGuard
+from .consumer_hash import canonical_frame_sha256, stamp_canonical_frame
 from .corporate_actions import (
     AdjustedCorporateActionBar,
     CorporateActionAdjustmentEngine,
@@ -34,10 +41,7 @@ from .models import (
     RecoverySeverity,
 )
 from .registry import RecoveryRegistry, registry
-from .replay_frame import (
-    CanonicalReplayFrameAdapter,
-    CanonicalReplayFrameResult,
-)
+from .replay_frame import CanonicalReplayFrameAdapter, CanonicalReplayFrameResult
 from .replay_parity import (
     ReplayEvaluation,
     ReplayParityAnalyzer,
@@ -62,18 +66,18 @@ from .security_entity import (
     SecurityEntityRecoveryEngine,
     export_security_entity_recovery,
 )
-from .security_timeline import (
-    SecurityIdentityRecord,
-    SecurityIdentityTimeline,
-)
+from .security_timeline import SecurityIdentityRecord, SecurityIdentityTimeline
 
 __all__ = [
+    "CONSUMER_CONTRACT_VERSION",
     "AdjustedCorporateActionBar",
     "CanonicalPreviewRow",
     "CanonicalRecoveryEngine",
     "CanonicalReplayAudit",
     "CanonicalReplayBar",
     "CanonicalReplayBuilder",
+    "CanonicalReplayConsumerAttestation",
+    "CanonicalReplayConsumerGuard",
     "CanonicalReplayFrameAdapter",
     "CanonicalReplayFrameResult",
     "CanonicalReplaySnapshot",
@@ -109,12 +113,15 @@ __all__ = [
     "SecurityIdentityRecord",
     "SecurityIdentityTimeline",
     "SourceSchema",
+    "canonical_frame_sha256",
     "canonical_replay_sha256",
     "discover_sources",
+    "export_consumer_attestations",
     "export_corporate_action_recovery",
     "export_corporate_action_replay_audit",
     "export_replay_parity",
     "export_schema_discovery",
     "export_security_entity_recovery",
     "registry",
+    "stamp_canonical_frame",
 ]
