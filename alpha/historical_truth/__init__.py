b21733cfed9a8286f6d6f95a62b8908ceba84aec
lanekart <rishabwahal@gmail.com>
2026-07-20 15:06:08 +0530

    Export archive manager and dataset registry

--- alpha/historical_truth/__init__.py
+++ alpha/historical_truth/__init__.py
@@ -1,3 +1,9 @@
+from alpha.historical_truth.manager import (
+    ArchiveTask,
+    DownloadSummary,
+    HistoricalArchiveManager,
+    TaskState,
+)
 from alpha.historical_truth.models import (
     ArchiveDataset,
     ArchiveRequest,
@@ -6,14 +12,23 @@ from alpha.historical_truth.models import (
     ValidationIssue,
     ValidationSeverity,
 )
+from alpha.historical_truth.plugins import NseBhavcopyPlugin
+from alpha.historical_truth.registry import ArchiveDatasetPlugin, DatasetRegistry
 from alpha.historical_truth.resumable import HistoricalTruthWarehouse
 
 __all__ = [
     "ArchiveDataset",
+    "ArchiveDatasetPlugin",
     "ArchiveRequest",
+    "ArchiveTask",
+    "DatasetRegistry",
+    "DownloadSummary",
+    "HistoricalArchiveManager",
     "HistoricalTruthWarehouse",
     "ManifestRecord",
     "ManifestStatus",
+    "NseBhavcopyPlugin",
+    "TaskState",
     "ValidationIssue",
     "ValidationSeverity",
 ]
